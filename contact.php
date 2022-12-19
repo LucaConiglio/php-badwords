aea315f0c0ad077cd66bb0892ff6bde92510a673
LucaConiglio <lucaconiglio.92@gmail.com>
2022-12-19 17:21:15 +0100

    terminato

--- contact.php
+++ contact.php
@@ -5,16 +5,26 @@
   $name = $_POST["username"];
   $delete = $_POST["delete"];
   $message = $_POST["message"];
+  ?>
 
-  echo '<h1 class="text-center p-3 text-info">Hai inserito</h1>';
-  echo '<div class="text-center">';
+  <h1 class="text-center p-3 text-info">Hai inserito</h1>
+  <div class="text-center">
+
+    <?php
   echo $name . "<br>" . $delete . "<br>" . $message . "<br>";
   echo "il paragrafo ha una lunghezza di" . strlen($message);
-  echo "<br>";
-  echo $newparagraph = str_replace($delete, "xxx", $message);
-  echo "<br>";
-  echo "il nuovo paragrafo ha lunghezza ";
-  echo strlen($newparagraph);
-  echo '</div>';
+  ?>
+
+  <br>
+
+  <?php echo $newparagraph = str_replace($delete, "xxx", $message); ?>
+  
+  <br>
+
+  il nuovo paragrafo ha lunghezza
+
+   <?php echo strlen($newparagraph); ?>
+
+  </div>
 
   
